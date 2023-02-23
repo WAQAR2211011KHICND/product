a52bd3925e32b4cc91c3477eae432e6a2126ae0f
Muhammad Waqar <mohammad.waqar@xloopdigital.com>
2023-02-23 13:03:55 +0500

    testcases done

--- src/test/java/com/example/product/ProductTests.java
+++ src/test/java/com/example/product/ProductTests.java
@@ -9,6 +9,12 @@ import org.mockito.Mock;
 
 import com.example.product.Entity.Product;
 import com.example.product.Repository.ProductRepository;
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.List;
 
 public class ProductTests {
 
@@ -115,43 +121,5 @@ public class ProductTests {
 		assertEquals(image, cut.getImage());
 		assertEquals(Price, cut.getPrice());
     }
-    
-
-
-    // @Test
-    // public void testsFindAllProduct(){
-    //     Long id = 10L;
-    //     String name = "Ruby Slippers";
-    //     String shortDescription = "Animpressivepair ofslippersfeaturingthousands ofreal rubies";
-    //     String longDescription = "Long Data";
-    //     String image = "https://raw.githubusercontent.com/jeff-lent/Alisnobba/main/Capstone/ActualRubyRubySlippers.jpg";
-    //     Long Price = 684750000L;
-
-    //     cut = Product.builder()
-    //                         .id(id)
-    //                         .name(name)
-    //                         .shortDescription(shortDescription)
-    //                         .longDescription(longDescription)
-    //                         .image(image)
-    //                         .price(Price)
-    //                         .build();
-
-    //     Product cut2 = Product.builder()
-    //                         .id(id+1L)
-    //                         .name(name)
-    //                         .shortDescription(shortDescription)
-    //                         .longDescription(longDescription)
-    //                         .image(image)
-    //                         .price(Price)
-    //                         .build();
         
-    //     given(thingRepository.findAll()).willReturn(List.of(cut1, cut2));
-
-        
-    // }
-
-
-
-
-    
 }
